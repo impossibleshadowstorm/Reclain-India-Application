c9dca91983650b4bc622c0832453e5bf2dae0c27
saurhub123 <Saurabhkumar26121999@gmail.com>
2023-09-29 00:24:17 +0530

    SignUp

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -7,9 +7,49 @@ import 'package:reclaim_india/utils/constants.dart';
 
 class AuthController extends GetxController {
   var loginLoadingStatus = false.obs;
+  var registerLoading = false.obs;
+  Future<int> signup({
+    required String userId,
+    required String password,
+    required String mobile,
+    required String address,
+    required String io,
+  }) async {
+    // 101-User exist
+    // 0-Error
+    // 1-Signup Successfully
+    registerLoading.value = true;
+    try {
+      final existingUser = FirebaseFirestore.instance
+          .collection(Constants.admin)
+          .doc(userId)
+          .get();
+      if (existingUser.exists) {
+        registerLoading.value = false;
+        return 101;
+      } else {
+        var hashPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        await FirebaseFirestore.instance
+            .collection(Constants.admin)
+            .doc(userId)
+            .set({
+          'address': address,
+          'mobile': mobile,
+          'investigating_officer': io,
+          'password': hashPassword,
+          'user_id': userId
+        });
+        return 1;
+      }
+    } catch (e) {
+      log(e);
+      return 0;
+    }
+  }
 
   Future<int> login({required String userId, required String password}) async {
-    // 101-User Doesn't exists
+    // 101-User Doesn't exist
     // 102-Password Doesn't match
     // 0-Error
     // 1-Login Successfully
